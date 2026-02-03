58ab1d390740b98901c89067958aac5c981d401a
Stefan Lucas <stefan.luks.as@gmail.com>
2026-02-03 09:32:40 -0300

    make base to system of pages render.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const icones = ["public/imagens/angular.png","public/imagens/aseprite.png","public/imagens/bootstrap.png","public/imagens/c++.png","public/imagens/corel.png","public/imagens/csharp.svg","public/imagens/css.png","public/imagens/django.png","public/imagens/electron.png","public/imagens/flask.png","public/imagens/flutter.webp","public/imagens/git.jpg","public/imagens/godot.png","public/imagens/html.png","public/imagens/java.png","public/imagens/js.png","public/imagens/Kivy.png","public/imagens/node.png","public/imagens/ps.png","public/imagens/py.webp","public/imagens/pygame.png","public/imagens/react.png","public/imagens/roblox-studio.png","public/imagens/ts.png","public/imagens/unity.png","public/imagens/vue.png"]
+import dados from "./dados.js";
 
 window.onload = () => {
     const opcoes = document.querySelectorAll(".opcao");
@@ -14,17 +14,17 @@ window.onload = () => {
                 render.appendChild(h1);
                 let lista = document.createElement("div");
                 lista.className = "lista-habilidades"
-                for(let i=0; i<icones.length; i++){
+                for(let i=0; i<dados.icones.length; i++){
                     let div = document.createElement("div");
                     div.className = "item";
                     let icon = new Image();
-                    icon.src = icones[i];
+                    icon.src = dados.icones[i];
                     div.appendChild(icon);
                     lista.appendChild(div);
                 }
                 render.appendChild(lista);
                 let btnJogar = document.createElement("div");
-                btnJogar.className = "btn-jogar";
+                btnJogar.className = "btn-jogar-h";
                 btnJogar.textContent = "Jogar";
                 render.appendChild(btnJogar);
                 let nave = document.createElement("img");
@@ -33,11 +33,103 @@ window.onload = () => {
                 render.appendChild(nave);
             }
         },
-        P: () => {
-
+        P: {
+            nome: "Projetos",
+            renderizar: () => {
+                let projetos = dados.projetos;
+                projetos.forEach(projeto => {
+                    let div = document.createElement("div");
+                    div.className = "projeto"
+                    let info = document.createElement("div");
+                    info.className = "info"
+                    let h1 = document.createElement("h1");
+                    h1.className = "lobster-two-bold";
+                    h1.textContent = projeto.nome;
+                    let p = document.createElement("p");
+                    p.className = "descricao";
+                    p.textContent = projeto.descricao;
+                    info.appendChild(h1);
+                    info.appendChild(p);
+                    div.appendChild(info);
+                    let btns = document.createElement("div");
+                    btns.className = "btns"
+                    let git = document.createElement("a");
+                    git.href = projeto.github;
+                    git.innerHTML = `<i class="bi bi-github"></i>`;
+                    let link = document.createElement("a");
+                    link.href = projeto.link;
+                    link.innerHTML = `<i class="bi bi-box-arrow-up-right"></i>`;
+                    btns.appendChild(link);
+                    btns.appendChild(git);
+                    div.appendChild(info);
+                    div.appendChild(btns);
+                    render.appendChild(div);
+                });
+            }
         },
-        T: () => {
-
+        T: {
+            nome: "Minhas Turmas",
+            renderizar: () => {
+                let h1 = document.createElement("div");
+                h1.className = "titulo lobster-two-regular ";
+                h1.textContent = "Minhas turmas";
+                render.appendChild(h1);
+            }
+        },
+        D: {
+            nome: "Dev Log's",
+            renderizar: () => {
+                let h1 = document.createElement("div");
+                h1.className = "titulo lobster-two-regular";
+                h1.textContent = "Dev log's";
+                render.appendChild(h1);
+            }
+        },
+        J: {
+            nome: "Meus jogos",
+            renderizar: () => {
+                let h1 = document.createElement("div");
+                h1.className = "titulo lobster-two-regular ";
+                h1.textContent = "Meus Jogos";
+                render.appendChild(h1);
+                let jogos = dados.jogos;
+                jogos.forEach(jogo => {
+                    let div = document.createElement("div");
+                    div.className = "jogo w-100 d-flex";
+                    let img = document.createElement("img");
+                    // img.style.height = img.style.width = "150px";
+                    img.src = jogo.imagem;
+                    div.appendChild(img);
+                    let info = document.createElement("div");
+                    info.className = "info";
+                    let h3 = document.createElement("h3");
+                    h3.textContent = jogo.nome;
+                    let descricao= document.createElement("p");
+                    descricao.textContent = jogo.descricao;
+                    let btns = document.createElement("div");
+                    btns.className = "tag-btns";
+                    btns.style.display = "flex";
+                    jogo.categorias.forEach(cat => {
+                        console.log(cat);
+                        let tag = document.createElement("div");
+                        tag.className = "tag";
+                        tag.textContent = cat;
+                        btns.appendChild(tag);
+                    });
+                    let btnJogar = document.createElement("div");
+                    btnJogar.className = "btn-jogar";
+                    btnJogar.textContent = "Jogar";
+                    btnJogar.addEventListener("click", () => {
+                        window.location.href = jogo.link;
+                    });
+                    btns.appendChild(btnJogar);
+                    info.appendChild(h3);
+                    info.appendChild(descricao);
+                    info.appendChild(btns);
+                    div.appendChild(info);
+                    render.appendChild(div);
+                })
+            }
         }
     }
 
---
+++ dados.js
@@ -0,0 +1,49 @@
+const dados = {
+    projetos: [
+        {
+            nome: "Tfan Studios",
+            descricao: "O TFAN Studios é uma plataforma de jogos autorais onde são publicados projetos desenvolvidos pelo autor e compilados na Unity para HTML5 / WebGL. Os jogos são disponibilizados gratuitamente, permitindo que qualquer pessoa jogue diretamente pelo navegador, sem necessidade de instalação.",
+            github: "https://github.com/stefanluks/tfanstudios",
+            link: "https://tfanstudios.com.br/",
+            imagem: "https://github.com/stefanluks/stefanluks/blob/main/imagens/tfanstudios.png"
+        }, {
+            nome: "Auto Plan",
+            descricao: "Aplicação em Electron que automatiza o cadastro de planos de aula no sistema do SENAI. O professor seleciona a turma e a disciplina, importa uma planilha XLSX e o sistema copia automaticamente as informações para a plataforma, reduzindo trabalho manual e erros.",
+            github: "https://github.com/stefanluks/AutoPlan",
+            link: "",
+            imagem: "https://github.com/stefanluks/stefanluks/blob/main/imagens/auto-plan.png"
+        }, {
+            nome: "Fliperama Senai",
+            descricao: "Projeto em ElectronJS que simula um sistema operacional de fliperama para Raspberry Pi 4. Os jogos são desenvolvidos pelos alunos na Unity e compilados para HTML5 / WebGL.",
+            github: "https://github.com/stefanluks/fliperama-senai",
+            link: "",
+            imagem: "https://github.com/stefanluks/stefanluks/blob/main/imagens/senai-box.png"
+        }],
+    jogos: [
+        {
+            nome: "Snake Of True - Cobra da Verdade",
+            descricao: "Um jogo jogo da cobrinha, com um diferencia aprender e praticar Álgebra Booleana de forma divertida e interativa.",
+            categorias: ["Educacional","classico","tabela verdade"],
+            link: "https://thestefanlucas.itch.io/snake-of-true-cobra-da-verdade",
+            imagem: "https://img.itch.zone/aW1nLzIyODM1NzA5LnBuZw==/315x250%23c/yd922R.png"
+        },
+        {
+            nome: "Boiwa",
+            descricao: "Boiwa the alien has crashed on a strange planet and now needs to defeat slimes.",
+            categorias: ["Platformer","Aventura","speed run"],
+            link: "https://thestefanlucas.itch.io/boiwa",
+            imagem: "https://img.itch.zone/aW1nLzIwMjMwNjA0LmpwZw==/315x250%23c/37iAbW.jpg"
+        },
+        {
+            nome: "The Paper Doll Fight",
+            descricao: "A little paper doll takes on the big school supply bosses.",
+            categorias: ["Luta","Ação","Boss-fight"],
+            link: "https://thestefanlucas.itch.io/the-paper-doll-figth",
+            imagem: "https://img.itch.zone/aW1nLzE5NjUyNTg5LmpwZw==/315x250%23c/N68%2Fb5.jpg"
+        },
+    ],
+    turmas: [],
+    icones: ["public/imagens/angular.png", "public/imagens/aseprite.png", "public/imagens/bootstrap.png", "public/imagens/c++.png", "public/imagens/corel.png", "public/imagens/csharp.svg", "public/imagens/css.png", "public/imagens/django.png", "public/imagens/electron.png", "public/imagens/flask.png", "public/imagens/flutter.webp", "public/imagens/git.jpg", "public/imagens/godot.png", "public/imagens/html.png", "public/imagens/java.png", "public/imagens/js.png", "public/imagens/Kivy.png", "public/imagens/node.png", "public/imagens/ps.png", "public/imagens/py.webp", "public/imagens/pygame.png", "public/imagens/react.png", "public/imagens/roblox-studio.png", "public/imagens/ts.png", "public/imagens/unity.png", "public/imagens/vue.png"]
+}
+
+export default dados;
